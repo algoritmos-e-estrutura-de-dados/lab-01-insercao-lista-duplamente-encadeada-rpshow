66032c1e450d16fbc3d945a145d77d3bdf6eddf6
Bruno Schilling <79983589+rpshow@users.noreply.github.com>
2021-11-13 09:55:20 -0300

    Update lista.py

--- lab01/lista.py
+++ lab01/lista.py
@@ -1,14 +1,10 @@
-
 class Node:
-
     def __init__(self, x):
         self.x = x
         self.next = None
         self.prev = None
 
-
 class Lista:
-
     def __init__(self):
         self.init = None
         self.tail = None
@@ -16,7 +12,6 @@ class Lista:
     def append(self, node):
         """
         Método para inserir um elemento no final
-
         :param node:
         :return:
         """
@@ -27,12 +22,11 @@ class Lista:
 
         self.tail.next = node
         node.prev = self.tail
-
+        self.tail = node
 
     def add(self, node):
         """
         Inserir um elemento sempre no inicio da lista
-
         :param node:
         :return:
         """
@@ -48,17 +42,20 @@ class Lista:
         str_aux = '['
         node_aux = self.init
         while(node_aux is not None):
-            str_aux += str(node_aux.x) + ','
+            str_aux += str(node_aux.x)
+            if(node_aux.next is not None):
+                str_aux += ','
             node_aux = node_aux.next
         str_aux += ']'
         return str_aux
 
-
 if __name__ == '__main__':
     lista = Lista()
-    lista.add(Node(x=27))
-    lista.add(Node(x=1))
+    lista.add(Node(x=10))
+    lista.add(Node(x=11))
+    lista.add(Node(x=12))
     print(lista)
-    lista.append(Node(x=5))
-    lista.append(Node(x=19))
+    lista.append(Node(x=7))
+    lista.append(Node(x=8))
+    lista.append(Node(x=9))
     print(lista)
